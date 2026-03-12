8d325704135a4e951cb95cccab03f9dd373bf4b5
Carlos <caguilarvillanueva68@gmail.com>
2026-03-12 18:46:36 -0500

    Conexion a la base datos y inserción de los datos completado

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Teslo-Shop
 Es una tienda online llamada Teslo Shop, hecha con Next.js + TypeScript + Tailwind, que muestra productos con animaciones suaves (menú, grid y tarjetas de producto con cambio de imagen al hacer hover).
+
+
+## Correr en dev
+
+1. Clonar el repositorio
+2. Crear una copia del ```.env.template``` y renombrarlo a ```.env``` y cambiar las variables de entorno.
+3. Instalar dependencias  ```npm install```
+4. Levantar la base de datos ```docker-compose up -d```
+5. Correr las migraciones de Prisma ```npx prisma migrate dev```
+6. Ejecutar seed ```npx tsx prisma/seed.ts```
+6. Correr el proyecto ```npm run dev```
+
---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { prisma } from '@/lib/prisma';
+import { initialData } from '../seed/seed';
+
+async function main() {
+
+  if (process.env.NODE_ENV === 'production') return;
+
+  // 1.- Borrar registros previos
+  //await Promise.all([
+  await prisma.productImages.deleteMany(),
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+  //])
+
+  const { categories, products } = initialData;
+  // Categorias
+
+  const categoriesData = categories.map((name) => ({ name }))
+
+  await prisma.category.createMany({
+    data: categoriesData
+  })
+
+  // Productos
+
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  } , {} as Record<string, string>)
+
+  products.forEach( async product => {
+
+    const { images, type, ...rest} = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type]
+      }
+    })
+
+    //Images
+    const imagesData = images.map( image => ({
+      url: image,
+      productId: dbProduct.id
+    }))
+
+    await prisma.productImages.createMany({
+      data: imagesData
+    })
+  });
+  
+
+
+
+  console.log('Seed ejecutado correctamente');
+}
+
+main();
